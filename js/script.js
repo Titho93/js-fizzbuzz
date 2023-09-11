c83731701d339f704ec72efd1e4d9292d8f62b68
Titho93 <titho10.93@gmail.com>
2023-09-11 16:39:09 +0200

    [fix]append bug

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ const squareContainer = document.querySelector('.squares');
 for(let i = 1; i <= 100; i++){
   const square = document.createElement('div');
   square.classList.add('square');
-  square.append(i);
 
   if ((!(i % 3)) && (!(i % 5))){
     square.classList.add('red');
-    square.textContent = 'Fizzbuzz';
+    square.append('Fizzbuzz');
   }else if(!(i % 3)){
     square.classList.add('green');
-    square.textContent = 'Fizz';
+    square.append('Fizz');
   }else if(!(i % 5)){
     square.classList.add('yellow');
-    square.textContent = 'Buzz';
+    square.append('Buzz');
+  }else {
+    square.append(i)
   }
-  
   
   squareContainer.append(square);
 }
