0b6336e103512f797031ee12ad5c2e418afcee27
Titho93 <titho10.93@gmail.com>
2023-09-11 14:38:30 +0200

    [add]if color

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ for(let i = 1; i <= 100; i++){
   const square = document.createElement('div');
   square.classList.add('square');
   square.append(i);
+
+  if(!(i % 3)){
+    square.classList.add('green');
+  }
+
+  if(!(i % 5)){
+    square.classList.add('yellow');
+  }
+
+  if(!(i % 15)){
+    square.classList.add('red');
+  }
   
   squareContainer.append(square);
 }
